04d03843bc7f01909f67ca93377cf6ef88f5269b
yo team <yo team>
2018-01-16 09:38:15 +0800

    moved files to root

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
-    console.log('Server received request. 🔥 ');
+    console.log('Server received request.');
     res.end('Hello World!');
 };
 
