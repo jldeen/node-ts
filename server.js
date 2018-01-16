f1770aa8846a4d22b6832eccde0dfa0cc0fd1db0
yo team <yo team>
2018-01-16 09:47:22 +0800

    demo change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request.');
-    res.end('Hello World!');
+    res.end('Hello Tech Summit Singapore!');
 };
 
 const server = http.createServer(handler);
